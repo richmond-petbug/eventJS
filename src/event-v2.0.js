69653afadf6fd1c856b33a80d0dce99730e59c64
richmond <richmond.petbug@gmail.com>
2012-06-04 20:46:58 +0800

    modify

--- src/event-v2.0.js
+++ src/event-v2.0.js
@@ -1,7 +1,8 @@
-/*
- * 
- * 
- * 
+/**
+ * @description Event静态对象
+ * @return {Object}
+ * @version 2.0
+ * @author 阿虫
  */
 var EVENT = (function(){
 
@@ -15,10 +16,18 @@ var EVENT = (function(){
         catch (e) {
         }
     };
-    
+    /**
+     * description W3C浏览器
+     */
     if (document.addEventListener) {
+    	/**
+    	 * @description 添加事件监听
+    	 * @param {Object} dom对象
+    	 * @param {String} 事件名称
+    	 * @param {Function} 事件监听函数
+    	 * @example EVENT.add(dom, "click", func);
+    	 */
         Handler.add = function(element, eventType, handler){
-        
             var addEvt = function(e){
                 var targetEl = e.currentTarget, relatedEl = e.relatedTarget;
                 
@@ -26,20 +35,18 @@ var EVENT = (function(){
                     var el = e.currentTarget;
                     if (Function.prototype.call) {
                         handler.call(el, e);
-                    }
-                    else {
+                    } else {
                         el._currentHandler = handler;
                         el._currentHandler(e);
                         el._currentHandler(null);
                     }
                 }
             };
-            // 添加 mouseenter & mouseleave 事件
+            // 为非IE添加 mouseenter & mouseleave 事件
             if (eventType == "mouseenter" || eventType == "mouseleave") {
                 eventType = (eventType == "mouseenter") ? "mouseover" : "mouseout";
                 element.addEventListener(eventType, addEvt, false);
-            }
-            else {
+            } else {
                 element.addEventListener(eventType, handler, false);
             }
         };
@@ -49,16 +56,17 @@ var EVENT = (function(){
         };
         
     }
+    /**
+     * description IE浏览器
+     */
     else if (document.attachEvent) {
         Handler.add = function(element, eventType, handler){
-        
             if (Handler._find(element, eventType, handler) != -1) {
                 return;
             }
             
             var wrappedHandler = function(e){
-                if (!e) 
-                    e = window.event;
+                if (!e) e = window.event;
                 
                 var event = {
                     _event: e,
@@ -87,8 +95,7 @@ var EVENT = (function(){
                 
                 if (Function.prototype.call) {
                     handler.call(element, event);
-                }
-                else {
+                } else {
                     element._currentHandler = handler;
                     element._currentHandler(event);
                     element._currentHandler(null);
@@ -107,12 +114,10 @@ var EVENT = (function(){
             var d = element.document || element;
             var w = d.parentWindow;
             var id = Handler._uid();
-            if (!w._allHandlers) 
-                w._allHandlers = {};
+            if (!w._allHandlers) w._allHandlers = {};
             w._allHandlers[id] = h;
             
-            if (!element._handlers) 
-                element._handlers = [];
+            if (!element._handlers) element._handlers = [];
             
             element._handlers.push(id);
             
@@ -124,14 +129,10 @@ var EVENT = (function(){
         
         Handler.remove = function(element, eventType, handler){
             var i = Handler._find(element, eventType, handler);
-            
-            if (i == -1) 
-                return;
+            if (i == -1) return;
             var d = element.document || element;
             var w = d.parentWindow;
-            
             var handlerId = element._handlers[i];
-            
             var h = w._allHandlers[handlerId];
             element.detachEvent("on" + eventType, h.wrappedHandler);
             element._handlers.splice(i, 1);
@@ -140,8 +141,7 @@ var EVENT = (function(){
         
         Handler._find = function(element, eventType, handler){
             var handlers = element._handlers;
-            if (!handlers) 
-                return -1;
+            if (!handlers) return -1;
             var d = element.document || element;
             var w = d.parentWindow;
             
